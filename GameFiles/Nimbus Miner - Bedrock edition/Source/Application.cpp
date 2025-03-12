13a1d75b7654a3078f52153480ba4b35d16cb854
Alexandre <Alexandre94.fr@gmail.com>
2025-03-12 10:11:37 +0100

    📦 ▸ Adding "OpenGL" + GLFW + Glew, the game when launched opens a windows with a white triangle in it
    Notes :
    - OpenGL is not a thing you can download. You can see it has the guy who transfers CPU code to the GPU and vice-versa.
    
    - The triangle was drawed by the legacy OpenGL code, not the modern one, this code is temporary, it was just to test if OpenGL was working.

---
+++ GameFiles/Nimbus Miner - Bedrock edition/Source/Application.cpp
@@ -0,0 +1,61 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include "../ExternalTools/ConsoleTextColorizer/ConsoleTextColorizer.h"
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    // Initialize the library
+    if (!glfwInit())
+        return -1;
+
+    // Create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(640, 480, "Nimbus miner - Bedrock edition", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+    
+    // Glew initialization
+    if (glewInit() != GLEW_OK)
+    {
+        //ConsoleTextColorizer::SetConsoleTextColor(WindowsTextColorEnum::Red);
+        std::cout << "ERROR ! The function 'glewInit' has been called before the GLFW context creation ";
+        std::cout << "(before this method 'glfwMakeContextCurrent()' call)" << std::endl;
+        //ConsoleTextColorizer::ResetConsoleTextColorToDefault();
+
+        return -1;
+    }
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // TEMP
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f, 0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
