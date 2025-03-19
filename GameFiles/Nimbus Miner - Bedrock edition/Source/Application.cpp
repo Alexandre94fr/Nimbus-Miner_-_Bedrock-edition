83c08663d0294d6e935532a0cd8c1c1e54243385
Alexandre <Alexandre94.fr@gmail.com>
2025-03-19 15:44:51 +0100

    ✒️ ▸ Adding VAO (Vertex Array Object) and a lot of documentation into the code (main(void)) (check desc)
    A VAO (Vertex Array Object)
    
    Contains the configuration for reading data from VBOs
    
    It does not contain the data itself, but records :
    - which VBOs are used
    - which glVertexAttribPointer are active
    - which glEnableVertexAttribArrays are activated
    - the Index Buffer(GL_ELEMENT_ARRAY_BUFFER) too

--- GameFiles/Nimbus Miner - Bedrock edition/Source/Application.cpp
+++ GameFiles/Nimbus Miner - Bedrock edition/Source/Application.cpp
@@ -179,8 +179,8 @@ int main(void)
     // In order to not have duplicates of the same vertices we identify each vertices with an index
     // After that we will use it to draw our geometrical form
 
-    // GL_ELEMENT_ARRAY_BUFFER is used below to indicate the buffer that we will also create below
-    // contains the indices of each element in the "other" (GL_ARRAY_BUFFER) buffer.
+    // GL_ELEMENT_ARRAY_BUFFER is used below to indicate the vertexBufferObjectID that we will also create below
+    // contains the indices of each element in the "other" (GL_ARRAY_BUFFER) vertexBufferObjectID. 
 
     unsigned int geometryIndices[] =
     {
@@ -188,26 +188,79 @@ int main(void)
         2, 3, 0  // Second triangle, up-right -> up-left -> down-left
     };
     
+    // Creating a VAO (Vertex Array Object)
+    //
+    // Contains the configuration for reading data from VBOs
+    // 
+    // It does not contain the data itself, but records :
+    // - which VBOs are used
+    // - which glVertexAttribPointer are active
+    // - which glEnableVertexAttribArrays are activated
+    // - the Index Buffer(GL_ELEMENT_ARRAY_BUFFER) too
+
+    // +--------------------------------+
+    // |    VAO | Vertex Array Object   |
+    // +--------------------------------+
+    // |  
+    // | +------------------------------+
+    // | |  VBO | Vertex Buffer Object  |
+    // | |                              |
+    // | |  (GL_ARRAY_BUFFER)           |
+    // | |  Contains the vertices       |
+    // | |  raw data                    |
+    // | +------------------------------+
+    // |
+    // | +------------------------------+
+    // | |  IBO | Index Buffer Object   |
+    // | |                              |
+    // | |  (GL_ELEMENT_ARRAY_BUFFER)   |
+    // | |  Contains indexes            |
+    // | |  that say "which vertex      |
+    // | |  to draw at what time"       |
+    // | +------------------------------+
+    // |
+    // +--------------------------------+
+
+    unsigned int vertexArrayObjectID;
+    glGenVertexArrays(1, &vertexArrayObjectID);
+    glBindVertexArray(vertexArrayObjectID);
+
     // Creating a VBO (Vertex Buffer Object), (have sent the data to the graphic card)
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    // Contains the raw data (vertex positions, colors, normals, UVs, etc.) we want to send to the GPU
+
+    unsigned int vertexBufferObjectID;
+    glGenBuffers(1, &vertexBufferObjectID);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjectID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 4 * 2, geometryPositions, GL_STATIC_DRAW);
 
-    glEnableVertexAttribArray(0);
     glVertexAttribPointer(
-        0, // The index of the attribute (example : if you want to bind the second attribute of your vertex you will pass 1)
-        2, // The number of data in your attribute (example : If you want three float in your attributes, you pass 3)
-        GL_FLOAT, // The type of each of your data
-        GL_FALSE,
-        2 * sizeof(float), // The bytes size of all attributes of this vertex
-        // (example : if you have a vertex that have 3 attributes and each of them weight 12 bytes, that's mean you pass 12 * 3 (36 bytes))
-        (void*)0
+        0,                  // - The index of the attribute (example : if you want to bind the second attribute of your vertex you will pass 1)
+        2,                  // - The number of data in your attribute (example : If you want three float in your attributes, you pass 3)
+        GL_FLOAT,           // - The type of each of your data
+        GL_FALSE,           // - Is the value of each data will be normalized by OpenGL
+        2 * sizeof(float),  // - The bytes size of all attributes of this vertex
+                            // (example : if you have a vertex that have 3 attributes and each of them weight 12 bytes,
+                            //            that's mean you pass 12 * 3 (36 bytes))
+        (void*)0            // - The byte size of this attribute
+                            // (example : if you have a vertex that have 2 attributes and the first one weight 8 bytes, and the second one 4, 
+                            //            when you will bind the 0 attribute will set pass (void*)8, and (void*)4 if you bind the 1 attribute)
     );
+    glEnableVertexAttribArray(0);
 
-    unsigned int indexBufferObject;
-    glGenBuffers(1, &indexBufferObject);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+    // The 'glEnableVertexAttribArray' function will also link* the VertexArrayObject and the VertexBufferObject
+    // that means we will only need to use one line of code to bind the two ( glBindVertexArray(vertexArrayObjectID); )
+
+    // *The link between VBO and VAO is not direct or permanent,
+    // it's just that when you call glVertexAttribPointer, OpenGL notes in the active VAO :
+    // "For attribute 0, I need to search in such-and-such a VBO, with such-and-such a format."
+
+
+    // Creating the IBO (Index Buffer Object)
+    // Contains indexes that say "which vertex to draw at what time" 
+
+    unsigned int indexBufferObjectID;       
+    glGenBuffers(1, &indexBufferObjectID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObjectID);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), geometryIndices, GL_STATIC_DRAW);
 
     ShaderProgram shaderProgram = ParseShader("Source/Shaders/Default.shader");
@@ -216,11 +269,10 @@ int main(void)
     MessageDebugger::PrintMessage(std::string("FRAGMENT SHADER :\n") + shaderProgram.FragmentShaderProgram);
 
     unsigned int shader = CreateShader(shaderProgram.VertexShaderProgram, shaderProgram.FragmentShaderProgram);
-    glUseProgram(shader);
 
     // Passing the color to the shader (to the 'u_Color' uniform variable)
+
     int shaderLocation = glGetUniformLocation(shader, "u_Color");
-    glUniform4f(shaderLocation, 0.2f, 0.2f, 0.8f, 1.0f);
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
@@ -228,7 +280,16 @@ int main(void)
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // We use nullptr because we already bind the indexBufferObject before
+        // Bind the geometry we will draw
+
+        glUseProgram(shader);
+        glUniform4f(shaderLocation, 0.2f, 0.2f, 0.8f, 1.0f);
+
+        glBindVertexArray(vertexArrayObjectID);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObjectID);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        // We use nullptr because we already bind the indexBufferObjectID before
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
